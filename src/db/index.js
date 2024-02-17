723f5383033c6865b66547a29c8134be2d2ba20b
axat0207 <axxatagrawal@gmail.com>
2024-02-17 22:53:11 +0530

    railway deploy 03 mongo_uri public

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ const connectDB = async ()=>{
     const DB_NAME = 'abtaxDB'
     console.log(`${process.env.MONGODB_URI}/${DB_NAME}`)
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`mongodb+srv://axxatagrawal:XjlW8XggzZ7jUfBi@cluster0.wir5xrk.mongodb.net/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
 
     }catch(err){
