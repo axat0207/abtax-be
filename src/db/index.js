c521d6221a6f9657c2e7385f9e425f831650907c
axat0207 <axxatagrawal@gmail.com>
2024-02-17 22:51:28 +0530

    host on railway 02

--- src/db/index.js
+++ src/db/index.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async ()=>{
     const DB_NAME = 'abtaxDB'
+    console.log(`${process.env.MONGODB_URI}/${DB_NAME}`)
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
